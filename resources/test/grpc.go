42c33018047afeffa4466e403807089eb905fe0a
Aiden Scandella <git@sca.ndella.com>
2018-07-24 17:19:21 -0700

    Add end-to-end native gRPC test
    Checking in api.pb.go to avoid needing protoc/protoc-gen-go and the grpc
    plugin just to run the tests, especially since this file will pretty
    much never change (just for tests).
    
    Currently hitting the "loopback" grpc code in the daemon which just
    spits out the bytes you give it, so it's acting as an echo service.
    
    Start breaking out the integration test runner into multiple files for
    clarity/sanity/navigation.

---
+++ resources/test/grpc.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/singnet/snet-daemon/blockchain"
+	"github.com/singnet/snet-daemon/resources/test/echoservice"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+func testGRPC(t *testing.T, h harness, port string) {
+	job := h.createSignedJob()
+	conn, err := grpc.Dial("127.0.0.1:"+port, grpc.WithInsecure())
+	require.NoError(t, err, "Unable to dial gRPC client")
+	defer conn.Close()
+
+	grpcClient := echoservice.NewEchoServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	ctx = metadata.AppendToOutgoingContext(
+		ctx,
+		blockchain.JobAddressHeader, job.address,
+		blockchain.JobSignatureHeader, job.signature,
+	)
+	r, err := grpcClient.Echo(ctx, &echoservice.EchoEnvelope{
+		Message: "from gRPC",
+	})
+	assert.NoError(t, err, "got error performing gRPC echo request")
+	assert.NotEmpty(t, r, "expected response body on successful echo gRPC request")
+	assert.Equal(t, r.GetMessage(), "from gRPC")
+
+	// Failure paths
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err = grpcClient.Echo(ctx, &echoservice.EchoEnvelope{
+		Message: "this request is missing headers",
+	})
+	assert.Error(t, err, "expected error without job headers")
+	assert.Empty(t, r)
+}

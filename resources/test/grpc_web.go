9eb076c07fa11980d1479bb11ec9cb3b178f03ca
Eric Kearns <erckrns@gmail.com>
2018-07-31 11:09:31 -0700

    Update gRPC-Web test

--- resources/test/grpc_web.go
+++ resources/test/grpc_web.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"encoding/binary"
 	"io/ioutil"
 	"net/http"
 	"testing"
 	"time"
 
+	"github.com/golang/protobuf/proto"
+	"github.com/singnet/snet-daemon/resources/test/echoservice"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -16,12 +18,19 @@ func testGRPCWeb(t *testing.T, h harness, port string) {
 	job := h.createSignedJob()
 	time.Sleep(time.Second)
 
+	req := &echoservice.EchoEnvelope{Message: "from gRPC-Web"}
+	payload, err := proto.Marshal(req)
+	assert.NoError(t, err, "Unable to marshal request")
+
+	body := make([]byte, 5+len(payload))
+	binary.BigEndian.PutUint32(body[1:5], uint32(len(payload)))
+	copy(body[5:], payload)
+
 	daemonHTTPAddr := "http://127.0.0.1:" + port
-	httpReq, err := http.NewRequest("POST", daemonHTTPAddr+"/FakeService/FakeMethod",
-		bytes.NewBuffer([]byte("\x00\x00\x00\x00\x13"+`{"hello":"goodbye"}`)))
+	httpReq, err := http.NewRequest("POST", daemonHTTPAddr+"/EchoService/Echo", bytes.NewBuffer(body))
 	require.NoError(t, err)
 
-	httpReq.Header.Set("content-type", "application/grpc-web+json")
+	httpReq.Header.Set("content-type", "application/grpc-web+proto")
 	httpReq.Header.Set("snet-job-address", job.address)
 	httpReq.Header.Set("snet-job-signature", job.signature)
 
@@ -35,7 +44,9 @@ func testGRPCWeb(t *testing.T, h harness, port string) {
 	assert.Empty(t, grpcStatus, "Got non-empty Grpc-Status code on response")
 
 	httpRespBytes, err := ioutil.ReadAll(httpResp.Body)
-	assert.NoError(t, err, "Unable to read HTP response body from daemon")
-	fmt.Print(string(httpRespBytes))
+	assert.NoError(t, err, "Unable to read HTTP response body from daemon")
 	assert.NotEmpty(t, httpRespBytes, "Expected response body from daemon")
+	resp := &echoservice.EchoEnvelope{}
+	proto.Unmarshal(httpRespBytes[5:httpRespBytes[4]+5], resp)
+	assert.Equal(t, resp.GetMessage(), "from gRPC-Web")
 }

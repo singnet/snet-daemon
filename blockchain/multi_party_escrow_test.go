93a2e1bc682ba4cf5b9d9b41db35c32c463003ac
Vitaly Bogdanov <vsbogd@gmail.com>
2018-10-16 18:41:33 +0300

    Fix Ethereum unit test to deploy and call contracts properly.

--- blockchain/multi_party_escrow_test.go
+++ blockchain/multi_party_escrow_test.go
@@ -1,4 +1,4 @@
-//go:generate abigen --abi ../resources/blockchain/node_modules/singularitynet-token-contracts/abi/SingularityNetToken.json --bin ../resources/blockchain/node_modules/singularitynet-token-contracts/bytecode/SingularityNetToken.json --pkg blockchain --type SingularityNetToken --out singularity_net_token_test.go
+//go:generate abigen --abi ../resources/blockchain/node_modules/singularitynet-token-contracts/abi/SingularityNetToken.json --bin ../resources/blockchain/node_modules/singularitynet-token-contracts/bytecode/SingularityNetToken.hex --pkg blockchain --type SingularityNetToken --out singularity_net_token_test.go
 
 package blockchain
 
@@ -50,35 +50,36 @@ func getSimulatedEthereumEnvironment() (env ethereumEnvironment) {
 }
 
 func getMultiPartyEscrowContract(deployer *bind.TransactOpts) (mpe *MultiPartyEscrow) {
-	tokenAddress, _, token, err := DeploySingularityNetToken(getTransactOpts(deployer), mpeTest.eth)
+	tokenAddress, _, _, err := DeploySingularityNetToken(estimateGas(deployer), mpeTest.eth)
 	if err != nil {
 		panic(fmt.Sprintf("Unable to deploy SingularityNetToken contract, error: %v", err))
 	}
 	mpeTest.eth.Commit()
 
-	fmt.Println("token address", AddressToHex(&tokenAddress))
-	n, err := token.BalanceOf(&bind.CallOpts{From: deployer.From}, deployer.From)
-	fmt.Println("results", n, err)
-
-	mpeAddress, _, mpe, err := DeployMultiPartyEscrow(getTransactOpts(deployer), mpeTest.eth, tokenAddress)
+	_, _, mpe, err = DeployMultiPartyEscrow(estimateGas(deployer), mpeTest.eth, tokenAddress)
 	if err != nil {
 		panic(fmt.Sprintf("Unable to deploy MultiPartyEscrow contract, error: %v", err))
 	}
 	mpeTest.eth.Commit()
 
-	fmt.Println("MPE address", AddressToHex(&mpeAddress))
-	t, err := mpe.Token(nil)
-	fmt.Println("MPE token address", AddressToHex(&t), err)
-
 	return mpe
 }
 
-func getTransactOpts(wallet *bind.TransactOpts) (opts *bind.TransactOpts) {
+func estimateGas(wallet *bind.TransactOpts) (opts *bind.TransactOpts) {
 	return &bind.TransactOpts{
 		From:     wallet.From,
 		Signer:   wallet.Signer,
 		Value:    nil,
-		GasLimit: 1000000,
+		GasLimit: 0,
+	}
+}
+
+func setGas(wallet *bind.TransactOpts, gasLimit uint64) (opts *bind.TransactOpts) {
+	return &bind.TransactOpts{
+		From:     wallet.From,
+		Signer:   wallet.Signer,
+		Value:    nil,
+		GasLimit: gasLimit,
 	}
 }
 
@@ -91,7 +92,7 @@ func TestMain(m *testing.M) {
 func TestDepositAndOpenChannel(t *testing.T) {
 
 	_, err := multiPartyEscrowContract.DepositAndOpenChannel(
-		getTransactOpts(mpeTest.clientWallet),
+		setGas(mpeTest.clientWallet, 100000),
 		mpeTest.serverWallet.From,
 		big.NewInt(1000),
 		big.NewInt(42),

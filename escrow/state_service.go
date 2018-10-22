8d009fab1c1e4d48b995c8dbb88fb9831ea04f93
Vitaly Bogdanov <vsbogd@gmail.com>
2018-10-22 14:26:43 +0300

    Return channel state via gRPC
    Check channel id signature to authenticate the caller, get latest
    channel state, check channel is opened by caller. Return latest channel
    state if it has signed amount. If not get previous channel state and
    return signed amount from it.

--- escrow/state_service.go
+++ escrow/state_service.go
@@ -2,13 +2,16 @@
 package escrow
 
 import (
-	"github.com/singnet/snet-daemon/blockchain"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"math/big"
 )
 
 type PaymentChannelStateService struct {
+	latest LatestPaymentStorage
+	closed ClosedPaymentStorage
 }
 
 func (service *PaymentChannelStateService) GetChannelState(context context.Context, request *ChannelStateRequest) (reply *ChannelStateReply, err error) {
@@ -16,11 +19,61 @@ func (service *PaymentChannelStateService) GetChannelState(context context.Conte
 		"context": context,
 		"request": request,
 	}).Debug("GetChannelState called")
+
+	channelIdBytes := request.GetChannelId()
+	signature := request.GetSignature()
+	sender, err := getSignerAddressFromMessage(channelIdBytes, signature)
+	if err != nil {
+		return nil, status.New(codes.Unauthenticated, "Incorrect signature")
+	}
+
+	channelId := bytesToBigInt(channelIdBytes)
+	channel, ok, err := service.latest.GetByID(channelId)
+	if err != nil {
+		return nil, status.New(codes.Internal, "Channel storage error")
+	}
+	if !ok {
+		return nil, status.Newf(codes.NotFound, "Channel is not found, channelId: %v", channelId)
+	}
+
+	if channel.Sender != sender {
+		return nil, status.New(codes.Unauthenticated, "Only channel sender can get latest channel state")
+	}
+
+	if channel.Signature != nil {
+		return &ChannelStateReply{
+			CurrentNonce: channel.Nonce,
+			CurrentValue: channel.FullAmount,
+			SignedNonce:  signed.Nonce,
+			SignedAmount: signed.AuthorizedAmount,
+			Signature:    signed.Signature,
+		}, nil
+	}
+
+	if channel.Nonce.Cmp(big.NewInt(0)) == 0 {
+		return &ChannelStateReply{
+			CurrentNonce: channel.Nonce,
+			CurrentValue: channel.FullAmount,
+		}, nil
+	}
+
+	prevNonce := (&big.Int{}).Sub(channel.Nonce, big.NewInt(1))
+	prevChannel, ok, err := service.closed.GetByIDAndNonce(channelId, prevNonce)
+	if err != nil {
+		return nil, status.New(codes.Internal, "Channel storage error")
+	}
+	if !ok {
+		return &ChannelStateReply{
+			CurrentNonce: channel.Nonce,
+			CurrentValue: channel.FullAmount,
+		}, nil
+	}
+
 	return &ChannelStateReply{
-		CurrentNonce: bigIntToBytes(big.NewInt(1)),
-		CurrentValue: bigIntToBytes(big.NewInt(2)),
-		SignedNonce:  bigIntToBytes(big.NewInt(3)),
-		SignedAmount: bigIntToBytes(big.NewInt(4)),
-		Signature:    blockchain.HexToBytes("0xde4e998341307b036e460b1cc1593ddefe2e9ea261bd6c3d75967b29b2c3d0a24969b4a32b099ae2eded90bbc213ad0a159a66af6d55be7e04f724ffa52ce3cc1b"),
+		CurrentNonce: channel.Nonce,
+		CurrentValue: channel.FullAmount,
+		SignedNonce:  prevChannel.Nonce,
+		SignedAmount: prevChannel.AuthorizedAmount,
+		Signature:    prevChannel.Signature,
 	}, nil
 }

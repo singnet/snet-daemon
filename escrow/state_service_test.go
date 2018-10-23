a522c0b07b9a34422bf00554d0a3097a084864fe
Vitaly Bogdanov <vsbogd@gmail.com>
2018-10-23 14:48:50 +0300

    Cover state service by unit tests.

--- escrow/state_service_test.go
+++ escrow/state_service_test.go
@@ -3,6 +3,7 @@ package escrow
 import (
 	"crypto/ecdsa"
 	"encoding/hex"
+	"errors"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/assert"
@@ -53,6 +54,10 @@ var stateServiceTest = func() stateServiceTestType {
 			Nonce:            big.NewInt(3),
 			AuthorizedAmount: big.NewInt(12345),
 		},
+		defaultRequest: &ChannelStateRequest{
+			ChannelId: bigIntToBytes(defaultChannelId),
+			Signature: getSignature(bigIntToBytes(defaultChannelId), senderPrivateKey),
+		},
 		defaultReply: &ChannelStateReply{
 			CurrentNonce:        bigIntToBytes(big.NewInt(3)),
 			CurrentSignedAmount: bigIntToBytes(big.NewInt(12345)),
@@ -62,34 +67,19 @@ var stateServiceTest = func() stateServiceTestType {
 }()
 
 func TestGetChannelState(t *testing.T) {
-	channelId := big.NewInt(42)
-	signature, err := hex.DecodeString("0504030201")
-	assert.Nil(t, err)
 	stateServiceTest.storageMock.Put(
-		&PaymentChannelKey{ID: channelId},
-		&PaymentChannelData{
-			Sender:           stateServiceTest.senderAddress,
-			Signature:        signature,
-			Nonce:            big.NewInt(3),
-			AuthorizedAmount: big.NewInt(12345),
-		},
+		stateServiceTest.defaultChannelKey,
+		stateServiceTest.defaultChannelData,
 	)
 	defer stateServiceTest.storageMock.Clear()
 
 	reply, err := stateServiceTest.service.GetChannelState(
 		nil,
-		&ChannelStateRequest{
-			ChannelId: bigIntToBytes(channelId),
-			Signature: getSignature(bigIntToBytes(channelId), stateServiceTest.senderPrivateKey),
-		},
+		stateServiceTest.defaultRequest,
 	)
 
 	assert.Nil(t, err)
-	assert.Equal(t, &ChannelStateReply{
-		CurrentNonce:        bigIntToBytes(big.NewInt(3)),
-		CurrentSignedAmount: bigIntToBytes(big.NewInt(12345)),
-		CurrentSignature:    signature,
-	}, reply)
+	assert.Equal(t, stateServiceTest.defaultReply, reply)
 }
 
 func TestGetChannelStateChannelIdIsNotPaddedByZero(t *testing.T) {
@@ -107,4 +97,86 @@ func TestGetChannelStateChannelIdIsNotPaddedByZero(t *testing.T) {
 
 	assert.Nil(t, err)
 	assert.Equal(t, stateServiceTest.defaultReply, reply)
+}
+
+func TestGetChannelStateChannelIdIncorrectSignature(t *testing.T) {
+	reply, err := stateServiceTest.service.GetChannelState(
+		nil,
+		&ChannelStateRequest{
+			ChannelId: bigIntToBytes(stateServiceTest.defaultChannelId),
+			Signature: []byte{0x00},
+		},
+	)
+
+	assert.Equal(t, errors.New("incorrect signature"), err)
+	assert.Nil(t, reply)
+}
+
+func TestGetChannelStateChannelStorageError(t *testing.T) {
+	stateServiceTest.storageMock.SetError(errors.New("storage error"))
+	defer stateServiceTest.storageMock.Clear()
+
+	reply, err := stateServiceTest.service.GetChannelState(nil, stateServiceTest.defaultRequest)
+
+	assert.Equal(t, errors.New("channel storage error"), err)
+	assert.Nil(t, reply)
+}
+
+func TestGetChannelStateChannelNotFound(t *testing.T) {
+	channelId := big.NewInt(42)
+	stateServiceTest.storageMock.Clear()
+
+	reply, err := stateServiceTest.service.GetChannelState(
+		nil,
+		&ChannelStateRequest{
+			ChannelId: bigIntToBytes(channelId),
+			Signature: getSignature(bigIntToBytes(channelId), stateServiceTest.senderPrivateKey),
+		},
+	)
+
+	assert.Equal(t, errors.New("channel is not found, channelId: 42"), err)
+	assert.Nil(t, reply)
+}
+
+func TestGetChannelStateIncorrectSender(t *testing.T) {
+	stateServiceTest.storageMock.Put(
+		stateServiceTest.defaultChannelKey,
+		stateServiceTest.defaultChannelData,
+	)
+	defer stateServiceTest.storageMock.Clear()
+
+	reply, err := stateServiceTest.service.GetChannelState(
+		nil,
+		&ChannelStateRequest{
+			ChannelId: bigIntToBytes(stateServiceTest.defaultChannelId),
+			Signature: getSignature(
+				bigIntToBytes(stateServiceTest.defaultChannelId),
+				generatePrivateKey()),
+		},
+	)
+
+	assert.Equal(t, errors.New("only channel sender can get latest channel state"), err)
+	assert.Nil(t, reply)
+}
+
+func TestGetChannelStateNoOperationsOnThisChannelYet(t *testing.T) {
+	channelData := stateServiceTest.defaultChannelData
+	channelData.AuthorizedAmount = nil
+	channelData.Signature = nil
+	stateServiceTest.storageMock.Put(
+		stateServiceTest.defaultChannelKey,
+		channelData,
+	)
+	defer stateServiceTest.storageMock.Clear()
+
+	reply, err := stateServiceTest.service.GetChannelState(
+		nil,
+		stateServiceTest.defaultRequest,
+	)
+
+	assert.Nil(t, err)
+	expectedReply := stateServiceTest.defaultReply
+	expectedReply.CurrentSignedAmount = nil
+	expectedReply.CurrentSignature = nil
+	assert.Equal(t, expectedReply, reply)
 }

73b1de0b45b4d4e8ff7101113ae39483a5fa7fae
Vitaly Bogdanov <vsbogd@gmail.com>
2018-10-24 15:46:26 +0300

    Update comment

--- codec/codec.go
+++ codec/codec.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/encoding"
-	_ "google.golang.org/grpc/encoding/proto" // guarantees correct codec registering order
+	_ "google.golang.org/grpc/encoding/proto" // ensure default "proto" codec is registered first
 )
 
 func init() {

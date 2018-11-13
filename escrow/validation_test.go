30ed11b7eafe2d48c95bd556d1573b3871a1a6a4
Vitaly Bogdanov <vsbogd@gmail.com>
2018-11-13 15:31:13 +0300

    Make validation test suite methods private.

--- escrow/validation_test.go
+++ escrow/validation_test.go
@@ -49,18 +49,18 @@ func (suite *ValidationTestSuite) SetupSuite() {
 	}
 }
 
-func (suite *ValidationTestSuite) Payment() *Payment {
+func (suite *ValidationTestSuite) payment() *Payment {
 	payment := &Payment{
 		Amount:             big.NewInt(12345),
 		ChannelID:          big.NewInt(42),
 		ChannelNonce:       big.NewInt(3),
 		MpeContractAddress: suite.mpeContractAddress,
 	}
-	suite.Sign(payment, suite.senderPrivateKey)
+	suite.sign(payment, suite.senderPrivateKey)
 	return payment
 }
 
-func (suite *ValidationTestSuite) Channel() *PaymentChannelData {
+func (suite *ValidationTestSuite) channel() *PaymentChannelData {
 	return &PaymentChannelData{
 		Nonce:            big.NewInt(3),
 		Sender:           suite.senderAddress,
@@ -73,7 +73,7 @@ func (suite *ValidationTestSuite) Channel() *PaymentChannelData {
 	}
 }
 
-func (suite *ValidationTestSuite) Sign(payment *Payment, privateKey *ecdsa.PrivateKey) {
+func (suite *ValidationTestSuite) sign(payment *Payment, privateKey *ecdsa.PrivateKey) {
 	message := bytes.Join([][]byte{
 		payment.MpeContractAddress.Bytes(),
 		bigIntToBytes(payment.ChannelID),
@@ -85,8 +85,8 @@ func (suite *ValidationTestSuite) Sign(payment *Payment, privateKey *ecdsa.Priva
 }
 
 func (suite *ValidationTestSuite) TestPaymentIsValid() {
-	payment := suite.Payment()
-	channel := suite.Channel()
+	payment := suite.payment()
+	channel := suite.channel()
 
 	err := suite.validator.Validate(payment, channel)
 
@@ -94,10 +94,10 @@ func (suite *ValidationTestSuite) TestPaymentIsValid() {
 }
 
 func (suite *ValidationTestSuite) TestValidatePaymentChannelNonce() {
-	payment := suite.Payment()
+	payment := suite.payment()
 	payment.ChannelNonce = big.NewInt(2)
-	suite.Sign(payment, suite.senderPrivateKey)
-	channel := suite.Channel()
+	suite.sign(payment, suite.senderPrivateKey)
+	channel := suite.channel()
 	channel.Nonce = big.NewInt(3)
 
 	err := suite.validator.Validate(payment, channel)
@@ -106,28 +106,28 @@ func (suite *ValidationTestSuite) TestValidatePaymentChannelNonce() {
 }
 
 func (suite *ValidationTestSuite) TestValidatePaymentIncorrectSignatureLength() {
-	payment := suite.Payment()
+	payment := suite.payment()
 	payment.Signature = blockchain.HexToBytes("0x0000")
 
-	err := suite.validator.Validate(payment, suite.Channel())
+	err := suite.validator.Validate(payment, suite.channel())
 
 	assert.Equal(suite.T(), NewPaymentError(Unauthenticated, "payment signature is not valid"), err)
 }
 
 func (suite *ValidationTestSuite) TestValidatePaymentIncorrectSignatureChecksum() {
-	payment := suite.Payment()
+	payment := suite.payment()
 	payment.Signature = blockchain.HexToBytes("0xa4d2ae6f3edd1f7fe77e4f6f78ba18d62e6093bcae01ef86d5de902d33662fa372011287ea2d8d8436d9db8a366f43480678df25453b484c67f80941ef2c05ef21")
 
-	err := suite.validator.Validate(payment, suite.Channel())
+	err := suite.validator.Validate(payment, suite.channel())
 
 	assert.Equal(suite.T(), NewPaymentError(Unauthenticated, "payment signature is not valid"), err)
 }
 
 func (suite *ValidationTestSuite) TestValidatePaymentIncorrectSigner() {
-	payment := suite.Payment()
+	payment := suite.payment()
 	payment.Signature = blockchain.HexToBytes("0xa4d2ae6f3edd1f7fe77e4f6f78ba18d62e6093bcae01ef86d5de902d33662fa372011287ea2d8d8436d9db8a366f43480678df25453b484c67f80941ef2c05ef01")
 
-	err := suite.validator.Validate(payment, suite.Channel())
+	err := suite.validator.Validate(payment, suite.channel())
 
 	assert.Equal(suite.T(), NewPaymentError(Unauthenticated, "payment is not signed by channel sender"), err)
 }
@@ -137,7 +137,7 @@ func (suite *ValidationTestSuite) TestValidatePaymentChannelCannotGetCurrentBloc
 		currentBlock: func() (*big.Int, error) { return nil, errors.New("blockchain error") },
 	}
 
-	err := validator.Validate(suite.Payment(), suite.Channel())
+	err := validator.Validate(suite.payment(), suite.channel())
 
 	assert.Equal(suite.T(), NewPaymentError(Internal, "cannot determine current block"), err)
 }
@@ -147,10 +147,10 @@ func (suite *ValidationTestSuite) TestValidatePaymentExpiredChannel() {
 		currentBlock:               func() (*big.Int, error) { return big.NewInt(99), nil },
 		paymentExpirationThreshold: func() *big.Int { return big.NewInt(0) },
 	}
-	channel := suite.Channel()
+	channel := suite.channel()
 	channel.Expiration = big.NewInt(99)
 
-	err := validator.Validate(suite.Payment(), channel)
+	err := validator.Validate(suite.payment(), channel)
 
 	assert.Equal(suite.T(), NewPaymentError(Unauthenticated, "payment channel is near to be expired, expiration time: 99, current block: 99, expiration threshold: 0"), err)
 }
@@ -160,22 +160,22 @@ func (suite *ValidationTestSuite) TestValidatePaymentChannelExpirationThreshold(
 		currentBlock:               func() (*big.Int, error) { return big.NewInt(98), nil },
 		paymentExpirationThreshold: func() *big.Int { return big.NewInt(1) },
 	}
-	channel := suite.Channel()
+	channel := suite.channel()
 	channel.Expiration = big.NewInt(99)
 
-	err := validator.Validate(suite.Payment(), channel)
+	err := validator.Validate(suite.payment(), channel)
 
 	assert.Equal(suite.T(), NewPaymentError(Unauthenticated, "payment channel is near to be expired, expiration time: 99, current block: 98, expiration threshold: 1"), err)
 }
 
 func (suite *ValidationTestSuite) TestValidatePaymentAmountIsTooBig() {
-	payment := suite.Payment()
+	payment := suite.payment()
 	payment.Amount = big.NewInt(12346)
-	suite.Sign(payment, suite.senderPrivateKey)
-	channel := suite.Channel()
+	suite.sign(payment, suite.senderPrivateKey)
+	channel := suite.channel()
 	channel.FullAmount = big.NewInt(12345)
 
-	err := suite.validator.Validate(payment, suite.Channel())
+	err := suite.validator.Validate(payment, suite.channel())
 
 	assert.Equal(suite.T(), NewPaymentError(Unauthenticated, "not enough tokens on payment channel, channel amount: 12345, payment amount: 12346"), err)
 }

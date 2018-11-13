9b37885fc26ed43ee25f4ec630fb785d4f48ad23
Vitaly Bogdanov <vsbogd@gmail.com>
2018-11-13 13:47:53 +0300

    Move signature validation tests to validation tests.

--- escrow/validation_test.go
+++ escrow/validation_test.go
@@ -179,3 +179,34 @@ func (suite *ValidationTestSuite) TestValidatePaymentAmountIsTooBig() {
 
 	assert.Equal(suite.T(), NewPaymentError(Unauthenticated, "not enough tokens on payment channel, channel amount: 12345, payment amount: 12346"), err)
 }
+
+func (suite *ValidationTestSuite) TestGetPublicKeyFromPayment() {
+	payment := Payment{
+		MpeContractAddress: suite.mpeContractAddress,
+		ChannelID:          big.NewInt(1789),
+		ChannelNonce:       big.NewInt(1917),
+		Amount:             big.NewInt(31415),
+		// message hash: 04cc38aa4a27976907ef7382182bc549957dc9d2e21eb73651ad6588d5cd4d8f
+		Signature: blockchain.HexToBytes("0xa4d2ae6f3edd1f7fe77e4f6f78ba18d62e6093bcae01ef86d5de902d33662fa372011287ea2d8d8436d9db8a366f43480678df25453b484c67f80941ef2c05ef01"),
+	}
+
+	address, err := getSignerAddressFromPayment(&payment)
+
+	assert.Nil(suite.T(), err, "Unexpected error: %v", err)
+	assert.Equal(suite.T(), blockchain.HexToAddress("0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef"), *address)
+}
+
+func (suite *ValidationTestSuite) TestGetPublicKeyFromPayment2() {
+	payment := Payment{
+		MpeContractAddress: blockchain.HexToAddress("0x39ee715b50e78a920120c1ded58b1a47f571ab75"),
+		ChannelID:          big.NewInt(1789),
+		ChannelNonce:       big.NewInt(1917),
+		Amount:             big.NewInt(31415),
+		Signature:          blockchain.HexToBytes("0xde4e998341307b036e460b1cc1593ddefe2e9ea261bd6c3d75967b29b2c3d0a24969b4a32b099ae2eded90bbc213ad0a159a66af6d55be7e04f724ffa52ce3cc1b"),
+	}
+
+	address, err := getSignerAddressFromPayment(&payment)
+
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), blockchain.HexToAddress("0x592E3C0f3B038A0D673F19a18a773F993d4b2610"), *address)
+}

a9c7e38ea7a7c1d265f78f7c46e6ddd51f6f5cae
anand <anand@singularitynet.io>
2019-03-22 15:48:14 +0530

    Remove references to mnemonic Key and Private key ( Daemon should not be doing any write operations on block chain)

--- snetd/cmd/flags.go
+++ snetd/cmd/flags.go
@@ -106,8 +106,7 @@ func init() {
 	vip.BindPFlag(config.BlockchainEnabledKey, serveCmdFlags.Lookup("blockchain"))
 
 	vip.BindPFlag(config.EthereumJsonRpcEndpointKey, serveCmdFlags.Lookup("ethereum-endpoint"))
-	vip.BindPFlag(config.HdwalletMnemonicKey, serveCmdFlags.Lookup("mnemonic"))
-	vip.BindPFlag(config.HdwalletIndexKey, serveCmdFlags.Lookup("wallet-index"))
+
 	vip.BindPFlag(config.PassthroughEnabledKey, serveCmdFlags.Lookup("passthrough"))
 	vip.BindPFlag(config.SSLCertPathKey, serveCmdFlags.Lookup("ssl-cert"))
 	vip.BindPFlag(config.SSLKeyPathKey, serveCmdFlags.Lookup("ssl-key"))

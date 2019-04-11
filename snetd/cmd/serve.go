3967b8a2673576ed9d83b0c697702d7d7fd26063
anand <anand@singularitynet.io>
2019-04-11 12:07:07 +0530

    https://github.com/singnet/snet-daemon/issues/209 changes made inside a method with a pointer receiver is visible to the caller whereas this is not the case in value receiver

--- snetd/cmd/serve.go
+++ snetd/cmd/serve.go
@@ -130,7 +130,7 @@ func newDaemon(components *Components) (daemon, error) {
 }
 
 
-func (d *daemon) start() {
+func (d daemon) start() {
 
 	var tlsConfig *tls.Config
 
@@ -221,7 +221,7 @@ func (d *daemon) start() {
 
 }
 
-func (d daemon) stop() {
+func (d *daemon) stop() {
 
 	if d.grpcServer != nil {
 		d.grpcServer.GracefulStop()

a3eaa9f2f2ae2b48ef03271e6ed6f548764e9938
anand <anand@singularitynet.io>
2019-05-23 15:44:15 +0530

    Bug fix on the block number change

---
+++ authutils/auth_service_test.go
@@ -0,0 +1,21 @@
+//  authutils package provides functions for all authentication and singature validation related operations
+package authutils
+
+import (
+	"github.com/magiconair/properties/assert"
+	"github.com/singnet/snet-daemon/config"
+	"math/big"
+	"testing"
+)
+
+func TestCompareWithLatestBlockNumber(t *testing.T) {
+	config.Vip().Set(config.EthereumJsonRpcEndpointKey, "https://ropsten.infura.io")
+	currentBlockNum, _ := CurrentBlock()
+	err := CompareBlockNumbers(currentBlockNum.Add(currentBlockNum, big.NewInt(13)))
+	assert.Equal(t, "difference between the latest block chain number and the block number passed is 13 ", err.Error())
+
+	currentBlockNum, _ = CurrentBlock()
+	err = CompareBlockNumbers(currentBlockNum.Add(currentBlockNum, big.NewInt(5)))
+	assert.Equal(t, nil, err)
+
+}

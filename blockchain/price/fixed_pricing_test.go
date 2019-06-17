e7685a337eedbb258d37bd72d5a2d7dc3e2aa3a0
anand <anand@singularitynet.io>
2019-06-17 18:53:18 +0530

    https://github.com/singnet/snet-daemon/issues/286 Added more test cases for Coverage

---
+++ blockchain/price/fixed_pricing_test.go
@@ -0,0 +1,20 @@
+package price
+
+import (
+	"github.com/singnet/snet-daemon/blockchain"
+	"github.com/stretchr/testify/assert"
+	"math/big"
+	"testing"
+
+	"github.com/singnet/snet-daemon/handler"
+)
+
+func TestFixedPrice_GetPrice(t *testing.T) {
+	metadata, _ := blockchain.ReadServiceMetaDataFromLocalFile("../../service_metadata.json")
+	grpcCtx := &handler.GrpcStreamContext{}
+	pricing,_ := InitPricing(metadata)
+
+	price,err := pricing.pricingTypes[0].GetPrice(grpcCtx)
+	assert.Equal(t,price,big.NewInt(10000000))
+	assert.Nil(t,err)
+}

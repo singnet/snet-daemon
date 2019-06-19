4940aa4fbacec8cb422fc88bef0dad8a20dbe5ba
anand <anand@singularitynet.io>
2019-06-19 13:29:36 +0530

    https://github.com/singnet/snet-daemon/issues/286 Use json string to construct metadata rather then reading from file

--- pricing/fixed_method_pricing_test.go
+++ pricing/fixed_method_pricing_test.go
@@ -11,10 +11,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var testJsonDataFixedMethodPrice = "{\"version\": 1, \"display_name\": \"Example1\", \"encoding\": \"grpc\", \"service_type\": \"grpc\", \"payment_expiration_threshold\": 40320, \"model_ipfs_hash\": \"QmQC9EoVdXRWmg8qm25Hkj4fG79YAgpNJCMDoCnknZ6VeJ\", \"mpe_address\": \"0x5C7a4290F6F8FF64c69eEffDFAFc8644A4Ec3a4E\", \"pricing\":{\"package_name\":\"example_service\",\"price_model\":\"fixed_price_per_method\",\"details\":[{\"service_name\":\"Calculator\",\"method_pricing\":[{\"method_name\":\"add\",\"price_in_cogs\":2},{\"method_name\":\"sub\",\"price_in_cogs\":1},{\"method_name\":\"div\",\"price_in_cogs\":2},{\"method_name\":\"mul\",\"price_in_cogs\":3}]},{\"service_name\":\"Calculator2\",\"method_pricing\":[{\"method_name\":\"add\",\"price_in_cogs\":2},{\"method_name\":\"sub\",\"price_in_cogs\":1},{\"method_name\":\"div\",\"price_in_cogs\":3},{\"method_name\":\"mul\",\"price_in_cogs\":2}]}]}, \"groups\": [{\"group_name\": \"default_group\", \"group_id\": \"nXzNEetD1kzU3PZqR4nHPS8erDkrUK0hN4iCBQ4vH5U=\", \"payment_address\": \"0xD6C6344f1D122dC6f4C1782A4622B683b9008081\"}], \"endpoints\": [{\"group_name\": \"default_group\", \"endpoint\": \"\"}]}"
 
 
 func TestFixedMethodPrice_initPricingData(t *testing.T) {
-	metadata,_ := blockchain.ReadServiceMetaDataFromLocalFile("../service_metadata_method_pricing.json")
+	metadata,_ := blockchain.InitServiceMetaDataFromJson(testJsonDataFixedMethodPrice)
 	grpcCtx := &handler.GrpcStreamContext{Info:&grpc.StreamServerInfo{FullMethod:"/example_service.Calculator/add"}}
 	pricing,_ := InitPricingStrategy(metadata)
 	price,err := pricing.GetPrice(grpcCtx)
--- pricing/fixed_pricing_test.go
+++ pricing/fixed_pricing_test.go
@@ -9,12 +9,14 @@ import (
 	"github.com/singnet/snet-daemon/handler"
 )
 
+var testJsonDataFixedPrice = "{\"version\": 1, \"display_name\": \"Example1\", \"encoding\": \"grpc\", \"service_type\": \"grpc\", \"payment_expiration_threshold\": 40320, \"model_ipfs_hash\": \"QmQC9EoVdXRWmg8qm25Hkj4fG79YAgpNJCMDoCnknZ6VeJ\", \"mpe_address\": \"0x5C7a4290F6F8FF64c69eEffDFAFc8644A4Ec3a4E\", \"pricing\": {\"price_model\": \"fixed_price\", \"price_in_cogs\": 12000000}, \"groups\": [{\"group_name\": \"default_group\", \"group_id\": \"nXzNEetD1kzU3PZqR4nHPS8erDkrUK0hN4iCBQ4vH5U=\", \"payment_address\": \"0xD6C6344f1D122dC6f4C1782A4622B683b9008081\"}], \"endpoints\": [{\"group_name\": \"default_group\", \"endpoint\": \"\"}]}"
+
 func TestFixedPrice_GetPrice(t *testing.T) {
-	metadata, _ := blockchain.ReadServiceMetaDataFromLocalFile("../service_metadata.json")
+	metadata, _ := blockchain.InitServiceMetaDataFromJson(testJsonDataFixedPrice)
 	grpcCtx := &handler.GrpcStreamContext{}
 	pricing,_ := InitPricingStrategy(metadata)
 
 	price,err := pricing.pricingTypes[0].GetPrice(grpcCtx)
-	assert.Equal(t,price,big.NewInt(10000000))
+	assert.Equal(t,price,big.NewInt(12000000))
 	assert.Nil(t,err)
 }

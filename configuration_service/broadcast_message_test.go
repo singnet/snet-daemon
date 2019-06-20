dc70567cf07fd63ea2e4527cb5eb6e2c28130fea
anand <anand@singularitynet.io>
2019-06-20 19:34:30 +0530

    https://github.com/singnet/snet-daemon/issues/298 We need some kind a trigger ( for example say if someone wants to stop the Daemon from processing requests) through the Operator UI , we need daemon heartbeat and the interceptors to react) accordingly Hence the proposal Define a publisher and all the Subscribers will receive the notification accordingly

---
+++ configuration_service/broadcast_message_test.go
@@ -0,0 +1,26 @@
+package configuration_service
+
+import (
+	"github.com/stretchr/testify/assert"
+
+	"testing"
+)
+
+func TestNewChannelBroadcaster(t *testing.T) {
+
+}
+
+func TestChannelBroadcaster_NewSubscriber(t *testing.T) {
+	broadcaster := NewChannelBroadcaster()
+	assert.NotNil(t,broadcaster)
+	channel1 := broadcaster.NewSubscriber()
+	channel2 := broadcaster.NewSubscriber()
+	assert.NotNil(t,channel1)
+	assert.NotNil(t,channel2)
+	broadcaster.trigger <- 1
+	msg1  := <- channel1
+	msg2  := <- channel2
+	assert.Equal(t,1,msg1)
+	assert.Equal(t,msg2,msg1)
+	close(broadcaster.trigger)
+}

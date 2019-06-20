180fb50f49c1cbdb02e4b84c28c753a2d7f0bf38
anand <anand@singularitynet.io>
2019-06-20 19:45:18 +0530

    Added more comments

--- configuration_service/broadcast_message_test.go
+++ configuration_service/broadcast_message_test.go
@@ -17,10 +17,13 @@ func TestChannelBroadcaster_NewSubscriber(t *testing.T) {
 	channel2 := broadcaster.NewSubscriber()
 	assert.NotNil(t,channel1)
 	assert.NotNil(t,channel2)
+	//Add a message to trigger
 	broadcaster.trigger <- 1
 	msg1  := <- channel1
 	msg2  := <- channel2
 	assert.Equal(t,1,msg1)
+	//Check if all the subscribers received the same message
 	assert.Equal(t,msg2,msg1)
 	close(broadcaster.trigger)
 }
+

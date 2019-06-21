9d662337a92edf6849bcc4e4b14b5608bc32d949
anand <anand@singularitynet.io>
2019-06-21 17:39:01 +0530

    https://github.com/singnet/snet-daemon/issues/29
    Provided definition of GetConfiguration()
    And added test cases

--- configuration_service/configuration_service.go
+++ configuration_service/configuration_service.go
@@ -12,6 +12,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
 	"math/big"
+	"sort"
 	"strings"
 )
 
@@ -23,17 +24,23 @@ type ConfigurationService struct {
 //TO DO Separate PRs will be submitted to implement all the function below
 func (service ConfigurationService) GetConfiguration(ctx context.Context, request *EmptyRequest) (response *ConfigurationResponse, err error) {
 	//Authentication checks
-	if err = service.authenticate("_GetConfiguration",request.Auth); err != nil {
+	if err = service.authenticate("_GetConfiguration", request.Auth); err != nil {
 		return nil, err
 	}
-
-	return nil, fmt.Errorf("work in progress")
+	schema, err := service.buildSchemaDetails()
+	if err != nil {
+		return nil, err
+	}
+	response = &ConfigurationResponse{}
+	response.Schema = schema
+	response.CurrentConfiguration = getCurrentConfig()
+	return response, nil
 }
 
 func (service ConfigurationService) UpdateConfiguration(ctx context.Context, request *UpdateRequest) (response *ConfigurationResponse, err error) {
 
 	//Authentication checks
-	if err = service.authenticate("_UpdateConfiguration",request.Auth); err != nil {
+	if err = service.authenticate("_UpdateConfiguration", request.Auth); err != nil {
 		return nil, err
 	}
 	return nil, fmt.Errorf("work in progress")
@@ -41,7 +48,7 @@ func (service ConfigurationService) UpdateConfiguration(ctx context.Context, req
 
 func (service ConfigurationService) StopProcessingRequests(ctx context.Context, request *EmptyRequest) (response *StatusResponse, err error) {
 	//Authentication checks
-	if err = service.authenticate("_StopProcessingRequests",request.Auth); err != nil {
+	if err = service.authenticate("_StopProcessingRequests", request.Auth); err != nil {
 		return nil, err
 	}
 	return nil, fmt.Errorf("work in progress")
@@ -49,7 +56,7 @@ func (service ConfigurationService) StopProcessingRequests(ctx context.Context, 
 
 func (service ConfigurationService) StartProcessingRequests(ctx context.Context, request *EmptyRequest) (response *StatusResponse, err error) {
 	//Authentication checks
-	if err = service.authenticate("_StartProcessingRequests",request.Auth); err != nil {
+	if err = service.authenticate("_StartProcessingRequests", request.Auth); err != nil {
 		return nil, err
 	}
 	return nil, fmt.Errorf("work in progress")
@@ -57,7 +64,7 @@ func (service ConfigurationService) StartProcessingRequests(ctx context.Context,
 
 func (service ConfigurationService) IsDaemonProcessingRequests(ctx context.Context, request *EmptyRequest) (response *StatusResponse, err error) {
 	//Authentication checks
-	if err = service.authenticate("_IsDaemonProcessingRequests",request.Auth); err != nil {
+	if err = service.authenticate("_IsDaemonProcessingRequests", request.Auth); err != nil {
 		return nil, err
 	}
 	return nil, fmt.Errorf("work in progress")
@@ -71,7 +78,7 @@ func (service ConfigurationService) authenticate(prefix string, auth *CallerAuth
 	}
 
 	//Check if the Signature is not Expired
-	if err = authutils.CompareWithLatestBlockNumber(big.NewInt(int64(auth.CurrentBlock)));err != nil {
+	if err = authutils.CompareWithLatestBlockNumber(big.NewInt(int64(auth.CurrentBlock))); err != nil {
 		return err
 	}
 
@@ -112,6 +119,7 @@ func NewConfigurationService() *ConfigurationService {
 
 	return service
 }
+
 //Message format has been agreed to be as the below ( prefix,block number,and authenticating address)
 func (service *ConfigurationService) getMessageBytes(prefixMessage string, blocknumber uint64) []byte {
 	message := bytes.Join([][]byte{
@@ -120,4 +128,75 @@ func (service *ConfigurationService) getMessageBytes(prefixMessage string, block
 		blockchain.HexToAddress(service.address).Bytes(),
 	}, nil)
 	return message
+}
+
+func (service *ConfigurationService) buildSchemaDetails() (schema *ConfigurationSchema, err error) {
+	schema = &ConfigurationSchema{}
+
+	detailsFromConfig, err := config.GetConfigurationSchema()
+	schema.Details = make([]*ConfigurationParameter, 0)
+	if err != nil {
+		return nil, err
+	}
+	for _, eachConfig := range detailsFromConfig {
+		schema.Details = append(schema.Details, convertToConfigurationParameter(eachConfig))
+	}
+	return schema, nil
+}
+
+func convertToConfigurationParameter(configSchema config.ConfigurationDetails) *ConfigurationParameter {
+	configParam := &ConfigurationParameter{
+		Name:          configSchema.Name,
+		DefaultValue:  configSchema.DefaultValue,
+		RestartDaemon: convertToUpdateAction(configSchema.RestartDaemon),
+		Section:       configSchema.Section,
+		Description:   configSchema.Description,
+		Type:          convertToConfigurationType(configSchema.Type),
+		Mandatory:     configSchema.Mandatory,
+		Editable:      configSchema.Editable,
+	}
+	return configParam
+}
+
+func convertToConfigurationType(value string) ConfigurationParameter_Type {
+
+	switch value {
+	case "string":
+		return ConfigurationParameter_STRING
+
+	case "int":
+		return ConfigurationParameter_INTEGER
+
+	case "url":
+		return ConfigurationParameter_URL
+
+	case "bool":
+		return ConfigurationParameter_BOOLEAN
+
+	case "address":
+		return ConfigurationParameter_ADDRESS
+
+	default:
+		return ConfigurationParameter_STRING
+	}
+
+}
+
+func convertToUpdateAction(value bool) ConfigurationParameter_UpdateAction {
+
+	if value {
+		return ConfigurationParameter_RESTART_REQUIRED
+	}
+	return ConfigurationParameter_NO_IMPACT
+}
+
+
+func getCurrentConfig() map[string]string {
+	currentConfigMap := make(map[string]string, len(config.Vip().AllKeys()))
+	keys := config.Vip().AllKeys()
+	sort.Strings(keys)
+	for _, key := range keys {
+		currentConfigMap[key] = config.GetString(key)
+	}
+	return currentConfigMap
 }

3d66c76dd2d1799618f0e121e9e6c13c107ca9ac
anandrgitnirman <NIrman$123>
2020-03-12 20:22:41 +0530

    #472 Currently Daemon supports only one authenticating User who can access the configuration service ability to see Daemon configuration , Stop Daemon , Pause Daemon , See if any requests are in progress , edit Daemon configurations are the functions supported by configuration service
    , make this as a list of user , this gives the service provider better options to configure who all can make access the configuration service

--- configuration_service/configuration_service_test.go
+++ configuration_service/configuration_service_test.go
@@ -2,96 +2,64 @@ package configuration_service
 
 import (
 	"bytes"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/singnet/snet-daemon/authutils"
 	"math/big"
-
 	"testing"
 
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/singnet/snet-daemon/authutils"
-	"github.com/singnet/snet-daemon/blockchain"
 	"github.com/singnet/snet-daemon/config"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestConfiguration_Service_checkAuthenticationAddress(t *testing.T) {
-	service := ConfigurationService{}
-	err := service.checkAuthenticationAddress("")
-	assert.Equal(t, err.Error(), "invalid hex address specified/missing for configuration 'authentication_address' ,this is a mandatory configuration required to be set up manually for remote updates")
 
-	service.address = "0x4Af41abf4c6a4633B1574f05e74b802cBF42a96e"
-
-	//Now set the authentication_address in Daemon
-	config.Vip().Set(config.AuthenticationAddress, "0x4Af41abf4c6a4633B1574f05e74b802cBF42a96e")
-
-	//Pass an invalid hex address
-	err = service.checkAuthenticationAddress("0x5f41abf4c6a4633B1574f05e74b802cBF42a96e")
-	assert.Equal(t, "0x5f41abf4c6a4633B1574f05e74b802cBF42a96e is an invalid hex Address", err.Error())
-
-	//Pass a valid address , but not the one set up for authentication
-	err = service.checkAuthenticationAddress("0xD6C6344f1D122dC6f4C1782A4622B683b9008081")
-	assert.Equal(t, "unauthorized access, 0xD6C6344f1D122dC6f4C1782A4622B683b9008081 is not authorized", err.Error())
-
-	//Pass the correct address to Authenticate
-	err = service.checkAuthenticationAddress("0x4Af41abf4c6a4633B1574f05e74b802cBF42a96e")
+	config.Vip().Set(config.AuthenticationAddresses, []string{"0x4Af41abf4c6a4633B1574f05e74b802cBF42a96e", "0x06A1D29e9FfA2415434A7A571235744F8DA2a514", "0x94d04332C4f5273feF69c4a52D24f42a3aF1F207"})
+	service := ConfigurationService{authenticationAddressList: getAuthenticationAddress()}
+	err := service.checkAuthenticationAddress(common.Address(common.BytesToAddress(common.FromHex("0x39ee715b50e78a920120C1dED58b1a47F571AB75"))))
+	assert.Equal(t, err.Error(), "unauthorized access, 0x39ee715b50e78a920120C1dED58b1a47F571AB75 is not authorized")
+	//Pass an invalid hex authenticationAddressList
+	err = service.checkAuthenticationAddress(common.Address(common.BytesToAddress(common.FromHex("0x4Af41abf4c6a4633B1574f05e74b802cBF42a96e"))))
 	assert.Nil(t, err)
 
 }
 
 func TestNewConfigurationService(t *testing.T) {
-	config.Vip().Set(config.AuthenticationAddress, "sdsdds")
+	config.Vip().Set(config.AuthenticationAddresses, "")
 	service := NewConfigurationService(nil)
-	assert.Equal(t, service.address, "")
-
-	config.Vip().Set(config.AuthenticationAddress, "0xD6C6344f1D122dC6f4C1782A4622B683b9008081")
+	assert.Equal(t, 0, len(service.authenticationAddressList))
+	config.Vip().Set(config.AuthenticationAddresses, []string{"0x4Af41abf4c6a4633B1574f05e74b802cBF42a96e", "0x06A1D29e9FfA2415434A7A571235744F8DA2a514", "0x94d04332C4f5273feF69c4a52D24f42a3aF1F207"})
 	service = NewConfigurationService(nil)
-	assert.Equal(t, service.address, "0xD6C6344f1D122dC6f4C1782A4622B683b9008081")
+	assert.Equal(t, 3, len(service.authenticationAddressList))
 }
 
 func TestConfigurationService_authenticate(t *testing.T) {
 	config.Vip().Set(config.BlockChainNetworkSelected, "ropsten")
+	service := NewConfigurationService(nil)
 	config.Validate()
+	privateKey, _ := crypto.GenerateKey()
 	currBlk, _ := authutils.CurrentBlock()
-	tests := []struct {
-		address string
-		auth    *CallerAuthentication
-		wantErr bool
-		prefix  string
-		message string
-	}{
-		{address: "0xD6C6344f1D122dC6f4C1782A4622B683b9008081",
-			auth: &CallerAuthentication{CurrentBlock: 2, UserAddress: "0xD6C6344f1D122dC6f4C1782A4622B683b9008081",
-				Signature: nil}, wantErr: true, message: "authentication failed as the signature passed has expired", prefix: ""},
-		{address: "0xD6C6344f1D122dC6f4C1782A4622B683b9008081",
-			auth: &CallerAuthentication{CurrentBlock: currBlk.Uint64(), UserAddress: "0xF6C6344f1D122dC6f4C1782A4622B683b9008081",
-				Signature: nil}, wantErr: true, message: "unauthorized access, 0xF6C6344f1D122dC6f4C1782A4622B683b9008081 is not authorized", prefix: ""},
-		{address: "0xD6C6344f1D122dC6f4C1782A4622B683b9008081",
-			auth: &CallerAuthentication{CurrentBlock: currBlk.Uint64(), UserAddress: "0xD6C6344f1D122dC6f4C1782A4622B683b9008081",
-				Signature: nil}, wantErr: true, message: "incorrect signature length", prefix: ""},
-	}
-	for _, tt := range tests {
-		t.Run(tt.address, func(t *testing.T) {
-			service := ConfigurationService{
-				address: tt.address,
-			}
-			err := service.authenticate(tt.prefix, tt.auth)
-			if tt.wantErr {
-				assert.Equal(t, tt.message, err.Error())
-			} else {
-				assert.Nil(t, err)
-			}
-		})
-	}
+	publicKey := crypto.PubkeyToAddress(privateKey.PublicKey)
+	msg := service.getMessageBytes("__GetConfiguration", uint64(currBlk.Int64()))
+	sig := authutils.GetSignature(msg, privateKey)
+	auth := &CallerAuthentication{UserAddress: publicKey.Hex(), CurrentBlock: uint64(currBlk.Int64()), Signature: sig}
+	err := service.authenticate("__GetConfiguration", auth)
+	assert.Contains(t, err.Error(), "unauthorized access")
+	config.Vip().Set(config.AuthenticationAddresses, []string{publicKey.Hex(), "0x06A1D29e9FfA2415434A7A571235744F8DA2a514", "0x94d04332C4f5273feF69c4a52D24f42a3aF1F207"})
+	service = NewConfigurationService(nil)
+	err = service.authenticate("__GetConfiguration", auth)
+	assert.Nil(t, err)
+
 }
 
 func TestConfigurationService_getMessageBytes(t *testing.T) {
-	service := ConfigurationService{
-		address: "0xD6C6344f1D122dC6f4C1782A4622B683b9008081",
-	}
+	config.Vip().Set(config.AuthenticationAddresses, []string{"0xD6C6344f1D122dC6f4C1782A4622B683b9008081", "0x06A1D29e9FfA2415434A7A571235744F8DA2a514", "0x94d04332C4f5273feF69c4a52D24f42a3aF1F207"})
+	service := NewConfigurationService(nil)
 	msg := service.getMessageBytes("ABC", 123)
 	assert.Equal(t, bytes.Join([][]byte{
 		[]byte("ABC"),
 		abi.U256(big.NewInt(int64(123))),
-		blockchain.HexToAddress("0xD6C6344f1D122dC6f4C1782A4622B683b9008081").Bytes(),
 	}, nil), msg)
 }
 
@@ -119,7 +87,7 @@ func Test_convertToUpdateAction(t *testing.T) {
 
 func Test_convertToConfigurationType(t *testing.T) {
 	assert.Equal(t, ConfigurationParameter_BOOLEAN, convertToConfigurationType("bool"))
-	assert.Equal(t, ConfigurationParameter_ADDRESS, convertToConfigurationType("address"))
+	assert.Equal(t, ConfigurationParameter_ADDRESS, convertToConfigurationType("authenticationAddressList"))
 	assert.Equal(t, ConfigurationParameter_INTEGER, convertToConfigurationType("int"))
 	assert.Equal(t, ConfigurationParameter_URL, convertToConfigurationType("url"))
 	assert.Equal(t, ConfigurationParameter_STRING, convertToConfigurationType("string"))
@@ -134,9 +102,9 @@ func Test_getCurrentConfig(t *testing.T) {
 }
 
 func TestConfigurationService_buildSchemaDetails(t *testing.T) {
-	service:= &ConfigurationService{}
+	service := &ConfigurationService{}
 	gotSchema, err := service.buildSchemaDetails()
-	assert.Nil(t,err)
+	assert.Nil(t, err)
 	assert.True(t, len(gotSchema.Details) > 2)
 
 }

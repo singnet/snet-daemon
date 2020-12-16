550afa2c5184af794e814d48cfec563e521afd6e
anandrgitnirman <NIrman$123>
2020-12-16 14:21:15 +0530

    #518  : fix mockable link

--- metrics/clients_test.go
+++ metrics/clients_test.go
@@ -26,15 +26,14 @@ const (
 )
 
 type clientImplHeartBeat struct {
-
 }
 
 // Check implements `service Health`.
-func (service *clientImplHeartBeat) Check( ctx context.Context, req *pb.HealthCheckRequest) (*pb.HealthCheckResponse, error) {
-		return &pb.HealthCheckResponse{Status:pb.HealthCheckResponse_SERVING},nil
+func (service *clientImplHeartBeat) Check(ctx context.Context, req *pb.HealthCheckRequest) (*pb.HealthCheckResponse, error) {
+	return &pb.HealthCheckResponse{Status: pb.HealthCheckResponse_SERVING}, nil
 }
 
-func (service *clientImplHeartBeat) Watch(*pb.HealthCheckRequest, pb.Health_WatchServer) (error) {
+func (service *clientImplHeartBeat) Watch(*pb.HealthCheckRequest, pb.Health_WatchServer) error {
 	return nil
 }
 
@@ -63,7 +62,7 @@ func Test_callgRPCServiceHeartbeat(t *testing.T) {
 	assert.False(t, err != nil)
 
 	assert.NotEqual(t, `{}`, string(heartbeat), "Service Heartbeat must not be empty.")
-	assert.Equal(t,heartbeat.String(),pb.HealthCheckResponse_SERVING.String())
+	assert.Equal(t, heartbeat.String(), pb.HealthCheckResponse_SERVING.String())
 
 	serviceURL = "localhost:26000"
 	heartbeat, err = callgRPCServiceHeartbeat(serviceURL)
@@ -71,31 +70,31 @@ func Test_callgRPCServiceHeartbeat(t *testing.T) {
 }
 
 func Test_callHTTPServiceHeartbeat(t *testing.T) {
-	serviceURL := "http://demo8325345.mockable.io/heartbeat"
+	serviceURL := "http://demo5343751.mockable.io/heartbeat"
 	heartbeat, err := callHTTPServiceHeartbeat(serviceURL)
 	assert.False(t, err != nil)
 	assert.NotEqual(t, string(heartbeat), `{}`, "Service Heartbeat must not be empty.")
 	assert.Equal(t, string(heartbeat), `{"serviceID":"SERVICE001", "status":"SERVING"}`,
 		"Unexpected service heartbeat")
 
-/*	var sHeartbeat pb.HeartbeatMsg
-	err = json.Unmarshal(heartbeat, &sHeartbeat)
-	assert.True(t, err != nil)
-	assert.Equal(t, sHeartbeat.ServiceID, "SERVICE001", "Unexpected service ID")
+	/*	var sHeartbeat pb.HeartbeatMsg
+		err = json.Unmarshal(heartbeat, &sHeartbeat)
+		assert.True(t, err != nil)
+		assert.Equal(t, sHeartbeat.ServiceID, "SERVICE001", "Unexpected service ID")
 
-*/	serviceURL = "http://demo8325345.mockable.io"
+	*/serviceURL = "http://demo8325345.mockable.io"
 	heartbeat, err = callHTTPServiceHeartbeat(serviceURL)
 	assert.True(t, err != nil)
 }
 
 func Test_callRegisterService(t *testing.T) {
-	serviceURL := "https://demo8325345.mockable.io/register"
+	serviceURL := "https://demo5343751.mockable.io/register"
 	daemonID := GetDaemonID()
 
 	result := callRegisterService(daemonID, serviceURL)
 	assert.Equal(t, true, result)
 
-	serviceURL = "https://demo8325345.mockable.io/registererror"
+	serviceURL = "https://demo5343751.mockable.io/registererror"
 	result = callRegisterService(daemonID, serviceURL)
 	assert.Equal(t, false, result)
 

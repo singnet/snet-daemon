63d4942ce066ffc60a8baae79ba1436cb97e1475
anandrgitnirman <NIrman$123>
2020-12-16 15:36:39 +0530

    #518 : FIx breaking mockable

--- snetd/cmd/components_test.go
+++ snetd/cmd/components_test.go
@@ -7,34 +7,33 @@ import (
 )
 
 func TestComponents_verifyMeteringConfigurations(t *testing.T) {
-	config.Vip().Set(config.MeteringEndPoint,"http://demo8325345.mockable.io")
+	config.Vip().Set(config.MeteringEndPoint, "http://demo5343751.mockable.io")
 
 	component := &Components{}
-	ok, err := component.verifyAuthenticationSetUpForFreeCall("http://demo8325345.mockable.io/verify",
+	ok, err := component.verifyAuthenticationSetUpForFreeCall("http://demo5343751.mockable.io/verify",
 		"testgroup")
-	ok, err = component.verifyAuthenticationSetUpForFreeCall("http://demo8325345.mockable.io/test","");
+	ok, err = component.verifyAuthenticationSetUpForFreeCall("http://demo5343751.mockable.io/test", "")
 	if err != nil {
-		assert.Equal(t,err.Error(),"you need a specify a valid private key 'pvt_key_for_metering' as part of service publication process.invalid length, need 256 bits")
-		assert.False(t,ok)
+		assert.Equal(t, err.Error(), "you need a specify a valid private key 'pvt_key_for_metering' as part of service publication process.invalid length, need 256 bits")
+		assert.False(t, ok)
 
 	}
-	config.Vip().Set(config.PvtKeyForMetering,"6996606c7854992c10d8cdc9a13d511a9d9db8ab8f21e59d6ac901a76367b36b")
-	ok, err = component.verifyAuthenticationSetUpForFreeCall("http://demo8325345.mockable.io/verify",
+	config.Vip().Set(config.PvtKeyForMetering, "6996606c7854992c10d8cdc9a13d511a9d9db8ab8f21e59d6ac901a76367b36b")
+	ok, err = component.verifyAuthenticationSetUpForFreeCall("http://demo5343751.mockable.io/verify",
 		"testgroup")
-	assert.Nil(t,err)
-	assert.True(t,ok)
+	assert.Nil(t, err)
+	assert.True(t, ok)
 
-
-	ok, err = component.verifyAuthenticationSetUpForFreeCall("http://demo8325345.mockable.io/badurl","");
+	ok, err = component.verifyAuthenticationSetUpForFreeCall("http://demo5343751.mockable.io/badurl", "")
 	if err != nil {
-		assert.Equal(t,err.Error(),"Service call failed with status code : 404 ")
-		assert.False(t,ok)
+		assert.Equal(t, err.Error(), "Service call failed with status code : 404 ")
+		assert.False(t, ok)
 
 	}
 
-	ok, err = component.verifyAuthenticationSetUpForFreeCall("http://demo8325345.mockable.io/failedresponse","")
+	ok, err = component.verifyAuthenticationSetUpForFreeCall("http://demo5343751.mockable.io/failedresponse", "")
 	if err != nil {
-		assert.Equal(t, "Error returned by by Metering Service http://demo8325345.mockable.io/verify Verification, "+
+		assert.Equal(t, "Error returned by by Metering Service http://demo5343751.mockable.io/verify Verification, "+
 			"pls check the pvt_key_for_metering set up. The public key in metering does not correspond to the private key in Daemon config.", err.Error())
 		assert.False(t, ok)
 	}

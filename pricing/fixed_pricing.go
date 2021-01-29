2989fd0e633834f32e4c0cb45d0fed4d596643fa
anandrgitnirman <NIrman$123>
2021-01-29 15:30:10 +0530

    #526 Rather than an Array , store the pricing type and associated PriceType instance in a map

--- pricing/fixed_pricing.go
+++ pricing/fixed_pricing.go
@@ -7,9 +7,13 @@ import (
 
 type FixedPrice struct {
 	//Value initialized from metaData
-	priceInCogs *big.Int;
+	priceInCogs *big.Int
 }
 
-func (priceType FixedPrice) GetPrice(GrpcContext *handler.GrpcStreamContext) (price *big.Int , err error) {
-	return priceType.priceInCogs,nil
+func (priceType FixedPrice) GetPrice(GrpcContext *handler.GrpcStreamContext) (price *big.Int, err error) {
+	return priceType.priceInCogs, nil
+}
+
+func (priceType FixedPrice) GetPriceType() string {
+	return FIXED_PRICING
 }

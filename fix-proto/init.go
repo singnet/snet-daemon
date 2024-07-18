14f1a8e549225f7c6112fb383356cba1776941ea
Semyon Novikov <semyon.novikov@singularitynet.io>
2024-07-18 17:49:03 +0300

    logs: ignore proto warning

--- fix-proto/init.go
+++ fix-proto/init.go
@@ -6,5 +6,5 @@ func init() {
 	// fixing the panic due to the fact that the libraries have the same proto files by name:
 	// github.com/ipfs/go-cid conflicts with etcd library
 	// https://protobuf.dev/reference/go/faq/#fix-namespace-conflict
-	os.Setenv("GOLANG_PROTOBUF_REGISTRATION_CONFLICT", "warn")
+	os.Setenv("GOLANG_PROTOBUF_REGISTRATION_CONFLICT", "ignore")
 }

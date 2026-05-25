2244755ae31740d63f401577386fb24df29a0857
Semyon Novikov <semyon.novikov@singularitynet.io>
2026-05-25 15:51:57 +0300

    bump deps (#683)
    * build: bump deps
    * fix: close body after heartbeat

--- token/token_service_api.go
+++ token/token_service_api.go
@@ -3,7 +3,7 @@ package token
 type PayLoad any
 type CustomToken any
 
-// Token.Manager interface is an API for creating and verifying tokens
+// Manager interface is an API for creating and verifying tokens
 type Manager interface {
 	CreateToken(key PayLoad, signer string) (token CustomToken, err error)
 	VerifyToken(token CustomToken, key PayLoad) (userAddress string, err error)
